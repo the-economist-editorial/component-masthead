2104a03e5c2d15175b5ba86e1fef161ee192b354
umbobabo <umbertobabini@economist.com>
2015-06-22 13:17:13 +0100

    New MH

--- example.js
+++ example.js
@@ -1,4 +1,9 @@
 import MastHead from './index';
+import ShareBar from './../component-sharebar';
 import React from 'react';
 
-export default (<MastHead title="This is the title" subtitle="This is the subtitle" />);
+export default (
+  <MastHead>
+    <ShareBar></ShareBar>
+  </MastHead>
+);
--- index.js
+++ index.js
@@ -2,34 +2,28 @@ import React from 'react';
 
 export default class MastHead extends React.Component {
 
-  // static defaultProps() {
-  //   return {
-  //     subtitle: 'Cooolll'
-  //   };
-  // }
-
   propTypes: {
-    title: React.propTypes.string.isRequired,
-    subtitle: React.propTypes.string
+    title: React.PropTypes.string,
+    subtitle: React.PropTypes.string,
   }
 
   constructor(props) {
     super(props);
   }
 
-
   render() {
     return (
       <div className="mnv-ec-masthead">
         <div className="mnv-ec-masthead-outer-wrap">
           <div className="mnv-ec-masthead-wrap">
-            <a href="http://www.economist.com" className="mnv-ec-masthead-logo svg-logo">
-              <img alt="The Economist" src="//cdn.static-economist.com/sites/all/themes/econfinal/images/svg/logo.svg" className="mh-logo"/>
+            <a href={this.props.logo.href} className="mnv-ec-masthead-logo svg-logo">
+              <img alt={this.props.logo.alt} src={this.props.logo.src} className="mh-logo"/>
             </a>
             <h1 className="mnv-ec-masthead-text">
               <span className="mnv-ec-masthead-title">{this.props.title}</span>
               <span className="mnv-ec-masthead-flytitle">{this.props.subtitle}</span>
             </h1>
+            {this.props.children}
           </div>
         </div>
       </div>
@@ -37,3 +31,11 @@ export default class MastHead extends React.Component {
   }
 
 }
+
+MastHead.defaultProps = {
+  logo: {
+    href: "http://www.economist.com",
+    alt: "The Economist",
+    src: "//cdn.static-economist.com/sites/all/themes/econfinal/images/svg/logo.svg"
+  }
+};
