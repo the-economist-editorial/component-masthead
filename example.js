b0c294113136f7e1165fb3dc0b51d91a03a231cd
umbobabo <umbertobabini@economist.com>
2015-06-26 10:41:31 +0100

    Removed Sticky ref from the example

--- example.js
+++ example.js
@@ -1,7 +1,6 @@
 import MastHead from './index';
 import ShareBar from './../component-sharebar';
 import React from 'react';
-import Sticky from 'react-sticky';
 
 export default (
   <MastHead title="This is the old" subtitle="MastHead"></MastHead>
