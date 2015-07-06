f20b7e14c83ddfc5c0a8d254ccd8f0b0db46692d
umbobabo <umbertobabini@economist.com>
2015-07-06 15:26:56 +0100

    Restored final version

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 import React from 'react';
 
-export default class Page extends React.Component {
+export default class MastHead extends React.Component {
+
+  propTypes: {
+    title: React.PropTypes.string,
+    subtitle: React.PropTypes.string,
+  }
 
   constructor(props) {
     super(props);
   }
 
+  static defaultProps = {
+    logo: {
+      href: "http://www.economist.com",
+      alt: "The Economist",
+      src: "//cdn.static-economist.com/sites/all/themes/econfinal/images/svg/logo.svg"
+    }
+  };
+
   render() {
     return (
-      <div>
-        {this.props.children}
+      <div className="mnv-ec-masthead">
+        <div className="mnv-ec-masthead-outer-wrap">
+          <div className="mnv-ec-masthead-wrap">
+            <a href={this.props.logo.href} className="mnv-ec-masthead-logo svg-logo">
+              <img alt={this.props.logo.alt} src={this.props.logo.src} className="mh-logo"/>
+            </a>
+            {this.props.title ?
+              <h1 className="mnv-ec-masthead-text">
+                <span className="mnv-ec-masthead-title">{this.props.title}</span>
+                <span className="mnv-ec-masthead-flytitle">{this.props.subtitle}</span>
+              </h1> : null }
+            {this.props.children}
+          </div>
+        </div>
       </div>
     );
   }
